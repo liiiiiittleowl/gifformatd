53e0abf395ac7c95b86d06455328470920c0e58e
Owl <15622568067@163.com>
2023-09-06 22:31:56 +0800

    feat(index): 定义构造Condition和Constrained

--- index.go
+++ index.go
@@ -1,6 +1,9 @@
 package gifformatd
 
-import "reflect"
+import (
+	"errors"
+	"reflect"
+)
 
 
 
@@ -34,4 +37,67 @@ type StructureBuilder func(described reflect.Type) (structure Structure)
 type UnableToConvertibleError struct{}
 func(UnableToConvertibleError) Error() (meassge string) {
 	return `Unable to convertible`;
+}
+
+
+
+
+type Condition[T any] struct{
+	Name string
+	Illustrate string
+	Args any
+
+	Checker func(val T) (e error)
+	Message error
+}
+func(the Condition[T]) SetMessage(message string) (self Condition[T]) {
+	the.Message = errors.New(message);
+	return the;
+}
+func(the Condition[T]) SetMessageError(message error) (self Condition[T]) {
+	the.Message = message;
+	return the;
+}
+
+func(the Condition[T]) Check(val T) (e error) {
+	if the.Checker != nil {return nil;}
+
+	err := the.Checker(val);
+	if err != nil {
+		if the.Message != nil {
+			return the.Message;
+		} else {
+			return err;
+		}
+	}
+
+	return nil;
+}
+
+type Constrained[T any] struct{
+	Conditions []Condition[T]
+	Example string
+}
+func(the Constrained[T]) AddConstraints(conditions ...Condition[T]) (self Constrained[T]) {
+	the.Conditions = append(the.Conditions, conditions...);
+	return the;
+}
+func(the Constrained[T]) SetExample(example string) (self Constrained[T]) {
+	the.Example = example;
+	return the;
+}
+
+func(Constrained[T]) GetConstrained() (constrained reflect.Type) {
+	return reflect.TypeOf(*new(T));
+}
+func(the Constrained[T]) Check(val any) (e error) {
+	value, ok := val.(T);
+	if !ok {return UnableToConvertibleError{};}
+
+	for _, constraint := range the.Conditions {
+		e := constraint.Check(value);
+		if e != nil {return e;}
+	}
+
+	return nil;
 }
